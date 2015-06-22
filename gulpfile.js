05c65ffde30b08e0d65a9c327d3e7817beedf737
Todd Anderson <bustardcelly@gmail.com>
2015-06-22 16:03:14 +0200

    inclusion of cssnext into dev and build process.

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,25 @@ var babelify = require('babelify');
 var reactify = require('reactify');
 var browserify = require('browserify');
 
+var cssnextify = require('gulp-cssnext');
+
 var srcDir = [process.cwd(), 'app'].join(path.sep);
 var distDir = [process.cwd(), 'dist'].join(path.sep);
 
-gulp.task('default', function() {
+gulp.task('styles', function() {
+  gulp.src([srcDir, 'style', 'index.css'].join(path.sep))
+      .pipe(cssnextify({
+        compress: true
+      }))
+      .pipe(gulp.dest([distDir, 'style'].join(path.sep)));
+});
+
+gulp.task('copy', function() {
+  gulp.src([srcDir, 'index.html'].join(path.sep))
+      .pipe(gulp.dest(distDir));
+});
+
+gulp.task('scripts', function() {
   var out = [distDir, 'script'].join(path.sep);
   var b = browserify({
     entries: [srcDir, 'script', 'index.js'].join(path.sep),
@@ -22,3 +37,7 @@ gulp.task('default', function() {
     .pipe(source('index.js'))
     .pipe(gulp.dest(out));
 });
+
+gulp.task('default', ['scripts', 'styles', 'copy'], function() {
+});
+
